da8aceec96da64afa4bae082e512e7bf640f6c5f
imac <kiarabarrientossalazar@gmail.com>
2023-12-05 14:41:19 -0600

    fragments implementados,prueba 0.2

--- app/src/main/java/com/example/pet_care/adapters/SlidePagerAdapter.java
+++ app/src/main/java/com/example/pet_care/adapters/SlidePagerAdapter.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.PagerAdapter;
 import com.example.pet_care.R;
@@ -13,60 +14,28 @@ import com.example.pet_care.fragments.FragmentStar2;
 import com.example.pet_care.fragments.FragmentStar3;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SlidePagerAdapter  extends RecyclerView.Adapter {
-    private List<Fragment> fragments = new ArrayList<>();
+    private final List<String> titles = Arrays.asList("Tab 1", "Tab 2", "Tab 3");
 
-    public SlidePagerAdapter() {
-        fragments.add(new FragmentStar1());
-        fragments.add(new FragmentStar2());
-        fragments.add(new FragmentStar3());
+    public SlidePagerAdapter(FragmentManager fm) {
+        super(fm);
     }
 
     @Override
-
-
-    public
-
-    int
-
-    getCount()
-
-    {
-        return fragments.size();
-    }
-    public Fragment getItem(int
-
-                                    position)
-
-    {
-        return fragments.get(position);
+    public Fragment getItem(int position) {
+        return FragmentStar1.newInstance(titles.get(position));
     }
 
     @Override
-
-
-    public
-
-    void
-
-    destroyItem(ViewGroup container, int position, Object object)
-
-    {
-        container.removeView((View) object);
+    public int getCount() {
+        return titles.size();
     }
 
     @Override
-
-
-    public
-
-    boolean
-
-    isViewFromObject(View view, Object object)
-
-    {
-        return view == object;
+    public CharSequence getPageTitle(int position) {
+        return titles.get(position);
     }
 }
