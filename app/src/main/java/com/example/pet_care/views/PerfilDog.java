a923cb143067031f1579ad65a0f9399fb785ac92
JontheJol <100542245+JontheJol@users.noreply.github.com>
2023-12-09 18:52:24 -0600

    dogo style

---
+++ app/src/main/java/com/example/pet_care/views/PerfilDog.java
@@ -0,0 +1,16 @@
+package com.example.pet_care.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pet_care.R;
+
+public class PerfilDog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_dog);
+    }
+}
