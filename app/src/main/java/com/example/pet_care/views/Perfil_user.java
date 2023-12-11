325af13a4fbf4af7efa1388278fe6037ada96dfc
JontheJol <100542245+JontheJol@users.noreply.github.com>
2023-12-10 20:36:23 -0600

    cracion de  perfil de usuario

---
+++ app/src/main/java/com/example/pet_care/views/Perfil_user.java
@@ -0,0 +1,14 @@
+package com.example.pet_care.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.pet_care.R;
+
+public class Perfil_user extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_user);
+    }
+}
