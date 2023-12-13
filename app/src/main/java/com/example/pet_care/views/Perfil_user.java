3b0dd87bbc23705a70ebdc4495da6bced8d5eb4f
JontheJol <100542245+JontheJol@users.noreply.github.com>
2023-12-13 15:06:15 -0600

    Base para lograr cambiar la contraseña en Perfil_user

--- app/src/main/java/com/example/pet_care/views/Perfil_user.java
+++ app/src/main/java/com/example/pet_care/views/Perfil_user.java
@@ -1,13 +1,16 @@
 package com.example.pet_care.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import com.example.pet_care.R;
 
 public class Perfil_user extends AppCompatActivity {
-
+TextView name,surname,email,
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +18,11 @@ public class Perfil_user extends AppCompatActivity {
         Intent a=getIntent();
         int userId=a.getIntExtra("id",-1);
         String id=String.valueOf(userId);
+        Toolbar toolbar;
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
     }
 }
