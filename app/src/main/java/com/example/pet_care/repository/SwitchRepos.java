f738bc7cedd4fd31eb9cdcfe17aee1a28aa03877
JontheJol <100542245+JontheJol@users.noreply.github.com>
2023-12-14 12:27:30 -0600

    safe switch hasta repo

---
+++ app/src/main/java/com/example/pet_care/repository/SwitchRepos.java
@@ -0,0 +1,59 @@
+package com.example.pet_care.repository;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.pet_care.models.SwitchModel;
+import com.example.pet_care.request.SwitchRequest;
+import com.example.pet_care.retrofit.request_1;
+import com.example.pet_care.views.Switch_buzzer;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+
+public class SwitchRepos {
+    public Retrofit retrofit;
+
+    void setRetrofit(){retrofit= request_1.getRetrofit();}
+    public MutableLiveData<SwitchModel> Switchchange(String Switch)
+    {
+        setRetrofit();
+        SwitchRequest switch_buzzer=retrofit.create(SwitchRequest.class);
+        Call<SwitchModel> SwitchModelCall= switch_buzzer.Switchchange(Switch);
+        MutableLiveData<SwitchModel> mutable=new MutableLiveData<>();
+        SwitchModelCall.enqueue(new Callback<SwitchModel>() {
+            @Override
+            public void onResponse(retrofit2.Call<SwitchModel> call, retrofit2.Response<SwitchModel> response) {
+                SwitchModel switchModel;
+                switch(response.code()) {
+                    case 422:
+                    case 404:
+                    case 400:
+                        switchModel= new SwitchModel();
+                        switchModel.code=String.valueOf(response.code());
+                        mutable.setValue(switchModel);
+                        break;
+                    case 200:
+                        switchModel=response.body();
+
+                        if(switchModel != null)
+                        {
+                            switchModel.setSwitch(response.body().getSwitch());
+                            switchModel.code=String.valueOf(response.code());
+                        }
+                        mutable.setValue(switchModel);
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onFailure(retrofit2.Call<SwitchModel> call, Throwable t) {
+
+            }
+        });
+
+        return mutable;
+    }
+
+}
