36dd765bd3927d3c78f588383c45d81db63614a3
JontheJol <100542245+JontheJol@users.noreply.github.com>
2023-12-15 08:20:16 -0600

    toolbar y eliminacion de Settings_dog

--- app/src/main/java/com/example/pet_care/views/Profile_Dog.java
+++ app/src/main/java/com/example/pet_care/views/Profile_Dog.java
@@ -1,6 +1,8 @@
 package com.example.pet_care.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -9,6 +11,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,6 +33,9 @@ public class Profile_Dog extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_dog);
+        Toolbar toolbar;
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         pet_nom=findViewById(R.id.pet_nom);
         recycler=findViewById(R.id.recyclerView);
@@ -62,5 +69,28 @@ public class Profile_Dog extends AppCompatActivity {
                 }
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id= item.getItemId();
+        if(id==R.id.item1)
+        {
+            startActivity(new Intent(Profile_Dog.this,Perfil_user.class));
+        }
+        else if(id==R.id.log_out)
+        {
+                startActivity(new Intent(Profile_Dog.this, Reg.class));
+        }
+        else if (id==R.id.home)
+        {
+            startActivity(new Intent(Profile_Dog.this, Home.class));
+        }
+        return true;
     }
 }
